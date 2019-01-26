7b7bd0fc005840b32dd144894bbc097a74346c73
Aleksandr <43940485+Shogun-1@users.noreply.github.com>
2019-01-26 23:26:37 +0300

    Adding comments

--- responses.h
+++ responses.h
@@ -7,23 +7,24 @@
 
 using namespace std;
 
+// 3 structs for queries, that require output
 struct BusesForStopResponse {
 	string stop;
-	map <string, vector <string>> stb;
+	map <string, vector <string>> stb; // stops to buses
 };
 
 ostream& operator << (ostream& os, const BusesForStopResponse& r);
 
 struct StopsForBusResponse {
 	string bus;
-	map <string, vector <string>> bts;
-	map <string, vector <string>> stb;
+	map <string, vector <string>> bts; // buses to stops
+	map <string, vector <string>> stb; // stops to buses
 };
 
 ostream& operator << (ostream& os, const StopsForBusResponse& r);
 
 struct AllBusesResponse {
-	map <string, vector <string>> bts;
+	map <string, vector <string>> bts; // buses to stops
 };
 
 ostream& operator << (ostream& os, const AllBusesResponse& r);
